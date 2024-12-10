e0b0fac2d2060b6171c333a7c484e0b2508cf8b5
geos <gsirotkin@yandex.ru>
2024-12-10 18:51:00 +0500

    create CatalogPosition model

---
+++ models/CatalogPosition.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const connection = require('../libs/connection');
+const CatalogLevel = require('./CatalogLevel');
+
+const Schema = new mongoose.Schema({
+  level: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: CatalogLevel,
+    required: 'не заполнено обязательное поле {PATH}',
+  },
+  title: {
+    type: String,
+    required: 'не заполнено обязательное поле {PATH}',
+  },
+  article: {
+    type: String,
+  },
+  image: { originalName: String, fileName: String },
+  isPublic: {
+    type: Boolean,
+    default: false,
+  },
+}, {
+  timestamps: true,
+});
+
+Schema.index(
+  {
+    title: 'text',
+  },
+  {
+    name: 'PositionSearchIndex',
+    default_language: 'russian',
+  },
+);
+
+module.exports = connection.model('CatalogPosition', Schema);
